712d6287de4e44f32db4cf01c825492d2e4aa21f
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-21 16:51:26 -0700

    fixing database, writing CLI methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,6 @@
 require_relative '../config/environment'
-require_relative "../lib/api_communicator.rb"
-require_relative "../lib/command_line_interface.rb"
+require_relative "../app/lib/api_communicator.rb"
+require_relative "../app/lib/command_line_interface.rb"
 
 
-
-puts "HELLO WORLD"
-
-get_random_pokemon
+CLI.new.start
